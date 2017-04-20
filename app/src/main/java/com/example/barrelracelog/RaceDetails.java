a01bbe649f2e6539a948bc1ac6a3d12fbdb84ab8
mitchell32 <mitch.radcliff@outlook.com>
2017-04-20 18:28:01 -0500

    Start of Race object class

---
+++ app/src/main/java/com/example/barrelracelog/RaceDetails.java
@@ -0,0 +1,19 @@
+package com.example.barrelracelog;
+
+/**
+ * Created by mitch on 4/20/2017.
+ */
+
+public class RaceDetails {
+    private int time;
+    private String position;
+    private String date;
+
+    public RaceDetails(int time, String position, String date){
+        this.time = time;
+        this.position = position;
+        this.date = date;
+    }
+
+
+}
