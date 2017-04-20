8dc52a91ad151dd4c41cfa9941803b7f5e7c5083
mitchell32 <mitch.radcliff@outlook.com>
2017-04-19 22:02:59 -0500

    Still need to add ability for text to be added into the custom grid

--- app/src/main/java/com/example/barrelracelog/HorseActivity.java
+++ app/src/main/java/com/example/barrelracelog/HorseActivity.java
@@ -1,15 +1,25 @@
 package com.example.barrelracelog;
 
+import android.app.Activity;
 import android.app.Dialog;
 import android.app.ListActivity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,69 +27,72 @@ import java.util.ArrayList;
  * Created by mitch on 4/19/2017.
  */
 
-public class HorseActivity extends AppCompatActivity {
-    //LIST OF ARRAY STRINGS THAT WILL SERVE AS HORSE NAMES
-    ArrayList<String> listItems = new ArrayList<String>();
+public class HorseActivity extends Activity {
 
-    //DEFINING A STRING ADAPTER WHICH WILL HANDLE THE DATA OF THE LISTVIEW
-    ArrayAdapter<String> adapter;
+    public class MyAdapter extends BaseAdapter {
 
-    int[] imageId;
+        final int NumberOfItem = 30;
+        private Bitmap[] bitmap = new Bitmap[NumberOfItem];
 
-    GridView grid;
+        private Context context;
+        private LayoutInflater layoutInflater;
 
+        MyAdapter(Context c) {
+            context = c;
+            layoutInflater = LayoutInflater.from(context);
+
+            //initial dummy bitmap
+            //using R.drawable.icon for all items
+            for(int i = 0; i < NumberOfItem; i++){
+                bitmap[i] = BitmapFactory.decodeResource(context.getResources(), R.mipmap.ic_launcher);
+            }
+        }
+
+        @Override
+        public int getCount(){
+            return bitmap.length;
+        }
+
+        @Override
+        public Object getItem(int position){
+            return bitmap[position];
+        }
+
+        @Override
+        public long getItemId(int position){
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent){
+
+            View grid;
+            if(convertView==null){
+                grid = new View(context);
+                grid = layoutInflater.inflate(R.layout.grid_single, null);
+            } else{
+                grid = convertView;
+            }
+
+            ImageView imageView = (ImageView)grid.findViewById(R.id.grid_image);
+            imageView.setImageBitmap(bitmap[position]);
+            TextView textView = (TextView)grid.findViewById(R.id.grid_text);
+            textView.setText(String.valueOf(position));
+
+            return grid;
+        }
+
+    }
+
+    GridView gridView;
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.horse_view);
+        gridView = (GridView)findViewById(R.id.grid);
 
-       // CustomGrid adapter = new CustomGrid(HorseActivity.this, "STEVE", imageId);
-        grid = (GridView) findViewById(R.id.grid);
-        grid.setAdapter(adapter);
-        grid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-
-            }
-        });
+        MyAdapter adapter = new MyAdapter(this);
+        gridView.setAdapter(adapter);
     }
-
-    //METHOD WHICH WILL HANDLE DYNAMIC INSERTION
-    public void addHorse(View v) {
-        final Dialog dialog = new Dialog(this);
-        dialog.setContentView(R.layout.add_horse_popup);
-        Button btnAdd = (Button) dialog.findViewById(R.id.add_horse);
-        Button btnCancel = (Button) dialog.findViewById(R.id.cancel_horse);
-
-
-        dialog.show();
-
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-
-            /* Check if add button is pressed before entering a name and make sure that
-            it does not get added to the horse name list
-             */
-            public void onClick(View v) {
-                EditText editText = (EditText) dialog.findViewById(R.id.edit_horse);
-                String horseName = editText.getText().toString();
-                listItems.add(horseName);
-                adapter.notifyDataSetChanged();
-                dialog.dismiss();
-            }
-
-
-        });
-
-        btnCancel.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-                dialog.dismiss();
-            }
-
-        });
-    }
-
-
 }
