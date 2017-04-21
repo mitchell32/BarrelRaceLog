b87238a3784be72261beb1385bdc3fd0fb5d721d
mitchell32 <mitch.radcliff@outlook.com>
2017-04-21 14:12:04 -0500

    Added additional notes String

--- app/src/main/java/com/example/barrelracelog/RaceDetails.java
+++ app/src/main/java/com/example/barrelracelog/RaceDetails.java
@@ -8,11 +8,13 @@ public class RaceDetails {
     private int time;
     private String position;
     private String date;
+    private String additionalNotes;
 
-    public RaceDetails(int time, String position, String date){
+    public RaceDetails(int time, String position, String date, String additionalNotes){
         this.time = time;
         this.position = position;
         this.date = date;
+        this.additionalNotes = additionalNotes;
     }
 
 
